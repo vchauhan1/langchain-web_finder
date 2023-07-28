14356f25d40c798cd67674d024bd34d290ec1624
VS <VS@localhost.localdomain>
2023-07-29 00:09:52 +0400

    first commit

--- web_finder.py
+++ web_finder.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import base64
 from langchain.llms import LlamaCpp
 from langchain.embeddings import LlamaCppEmbeddings
 from langchain.callbacks.base import BaseCallbackHandler
@@ -30,6 +31,29 @@ def _llm_specifications():
     web_finder = WebResearchRetriever.from_llm(vectorstore=vectorstore_public, llm=llm, search=search, num_search_results=2)
     return web_finder, llm
 
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_background_image(image_path):
+    image_url = get_base64_of_bin_file(image_path)
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/png;base64,{image_url});
+            background-attachment: fixed;
+            background-size: cover;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+local_image_path = "logo/internet.jpeg"
+set_background_image(local_image_path)
+st.info("`A bot that can find the answers over internet and summarize the web pages.`")
+st.header("`Web Finder`")
 class StreamOutputHandler(BaseCallbackHandler):
     def __init__(self, container, initial_text=""):
         self.container = container
@@ -52,10 +76,6 @@ class ContextRetrievalVisualizer(BaseCallbackHandler):
             source = doc.metadata["source"]
             self.container.write(f"**Results from {source}**")
             self.container.text(doc.page_content)
-
-st.info("`A bot that can find the answers over internet and summarize the web pages.`")
-st.sidebar.image("logo/internet.jpeg")
-st.header("`Web Finder`")
 
 web_finder, llm = _llm_specifications()
 question = st.text_input("`Ask a question:`")
